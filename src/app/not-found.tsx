740dc4e65746d1fc0cc42098c6a8be21f0a3c76f
hleyf <hleyf@hotmail.com>
2024-07-08 14:29:09 +0100

    route fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -21,7 +21,7 @@ export default function NotFound() {
                                 ></span>
 
                                 <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-                                    <Link href="/">Go Home</Link>
+                                    <Link href="/dashboard">Go Home</Link>
                                 </span>
                             </a>
                         </button>
